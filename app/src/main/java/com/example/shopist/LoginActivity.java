2a26f5b81f3c4269478b4f07725b36bd9bf06a35
rafaeljcamara <rafaelcamarac@gmail.com>
2021-03-21 14:25:38 +0000

    Add activity flow, from login to signup, and server login and signup requests.

--- app/src/main/java/com/example/shopist/LoginActivity.java
+++ app/src/main/java/com/example/shopist/LoginActivity.java
@@ -1,15 +1,129 @@
 package com.example.shopist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL="http://10.0.2.2:3000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        initRetrofit();
+        addSettings();
+    }
+
+    private void initRetrofit(){
+        //instantiate retrofit settings
+        retrofit  = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+    }
+
+    private void addSettings(){
+        addButtonSettings();
+        addSignupLinkSettings();
+        mainPageLinkSettings();
+    }
+
+    private void addButtonSettings(){
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleServerRequest();
+            }
+        });
+    }
+
+    private void handleServerRequest() {
+        HashMap<String, String> map = new HashMap<>();
+
+        //get email
+        EditText emailComponent = findViewById(R.id.emailLogin);
+        String email = emailComponent.getText().toString();
+
+        //get password
+        EditText passwordComponent = findViewById(R.id.passwordLogin);
+        String password = passwordComponent.getText().toString();
+
+        map.put("email", email);
+        map.put("password", password);
+
+        Call<ServerData> call = retrofitInterface.executeLogin(map);
+        call.enqueue(new Callback<ServerData>() {
+            //when the server responds to our request
+            @Override
+            public void onResponse(Call<ServerData> call, Response<ServerData> response) {
+                if (response.code() == 200) {
+                    //server success (credentials match)
+                    //move to main activity screen
+                    ServerData userInfoServer = response.body();
+                    String userInfo = userInfoServer.getName() + " - " + userInfoServer.getEmail();
+                    Toast.makeText(LoginActivity.this, userInfo, Toast.LENGTH_LONG).show();
+                } else if (response.code() == 404) {
+                    //no matching credentials
+                    Toast.makeText(LoginActivity.this, "Wrong credentials!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            //when the server fails to respond to our request
+            @Override
+            public void onFailure(Call<ServerData> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, "SERVER ERROR! Please try again later.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void addSignupLinkSettings(){
+        TextView signupLink = findViewById(R.id.signupLink);
+        signupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(LoginActivity.this, "Signup link", Toast.LENGTH_SHORT).show();
+                //go to signup activity
+
+                //create a new intent
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                //start activity targeted in the intent
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void mainPageLinkSettings(){
+        TextView mainpageLink = findViewById(R.id.gotoMainPageLink);
+        mainpageLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(LoginActivity.this, "Main page link", Toast.LENGTH_SHORT).show();
+                //go to main page activity
+
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/shopist/SignupActivity.java
+++ app/src/main/java/com/example/shopist/SignupActivity.java
@@ -1,15 +1,107 @@
 package com.example.shopist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class SignupActivity extends AppCompatActivity {
+
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "http://10.0.2.2:3000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        initRetrofit();
+        addSettings();
+    }
+
+    private void initRetrofit() {
+        //instantiate retrofit object
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+    }
+
+    private void addSettings() {
+        addButtonSettings();
+    }
+
+    private void addButtonSettings() {
+        Button signupButton = findViewById(R.id.signupButton);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleServerRequest();
+            }
+        });
+    }
+
+    private void handleServerRequest() {
+        HashMap<String, String> map = getFormInfo();
+        Call<Void> call = retrofitInterface.executeSignup(map);
+        call.enqueue(new Callback<Void>() {
+            //when the server responds to our request
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.code() == 200) {
+                    //server success
+                    //go to login activity with intent success message
+                    String success_message = "Successfully signed up!";
+                    Toast.makeText(SignupActivity.this, success_message, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            //when the server fails to respond to our request
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(SignupActivity.this, "SERVER ERROR! Please try again later.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private HashMap<String, String> getFormInfo() {
+        HashMap<String, String> map = new HashMap<>();
+
+        //get username
+        EditText usernameComponent = findViewById(R.id.usernameSignup);
+        String username = usernameComponent.getText().toString();
+        map.put("username",username);
+
+        //get email
+        EditText emailComponent = findViewById(R.id.emailSignup);
+        String email = emailComponent.getText().toString();
+        map.put("email",email);
+
+        //get first password
+        EditText passwordComponent = findViewById(R.id.passwordSignup);
+        String password = passwordComponent.getText().toString();
+        map.put("password",password);
+
+        //get confirm password
+        EditText confirmPasswordComponent = findViewById(R.id.confirmPasswordSignup);
+        String confirmPassword = confirmPasswordComponent.getText().toString();
+        map.put("confirmPassword",confirmPassword);
+
+        return map;
     }
 
 }
