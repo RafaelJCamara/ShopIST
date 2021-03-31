e9ab6dceb406fe3967b61a997f608ac97b7d6732
rafaeljcamara <rafaelcamarac@gmail.com>
2021-03-31 11:06:20 +0100

    Add create and get shopping lists logic.

--- app/src/main/java/com/example/shopist/MainActivity.java
+++ app/src/main/java/com/example/shopist/MainActivity.java
@@ -42,14 +42,29 @@ public class MainActivity extends AppCompatActivity {
         shoppingList = new ArrayList<String>();
         //LIST OPERATIONS
         listOperations();
-
         initRetrofit();
         addSettings();
     }
 
     private void listOperations(){
         pantryListSettings();
-//        shoppingListSettings();
+        shoppingListSettings();
+    }
+
+    private void initRetrofit(){
+        //instantiate retrofit settings
+        retrofit  = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+    }
+
+    private void addSettings(){
+        fillTextView();
+        addLogoutButtonLogic();
+        addPantryListLogic();
+        addShoppingListLogic();
     }
 
     private void pantryListSettings(){
@@ -92,14 +107,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 //                String itemSelected = listToDo.getItemAtPosition(position).toString();
-                Toast.makeText(getApplicationContext(),"Product clicked!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Shopping list clicked!",Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void shoppingSettings(){
         //get list view
-        shoppingLists = findViewById(R.id.pantryList);
+        shoppingLists = findViewById(R.id.shoppingList);
 
         //create list adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
@@ -117,20 +132,17 @@ public class MainActivity extends AppCompatActivity {
         shoppingLists.setAdapter(adapter);
     }
 
-
-    private void initRetrofit(){
-        //instantiate retrofit settings
-        retrofit  = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        retrofitInterface = retrofit.create(RetrofitInterface.class);
-    }
-
-    private void addSettings(){
-        fillTextView();
-        addLogoutButtonLogic();
-        addPantryListLogic();
+    private void fillTextView(){
+        TextView textView = findViewById(R.id.textView);
+        String username = getIntent().getStringExtra("username");
+        String email = getIntent().getStringExtra("email");
+        String info = "Welcome, ";
+        if(username.length()!=0){
+            info+=username;
+        }else{
+            info+="anonymous person";
+        }
+        textView.setText(info);
     }
 
     private void addLogoutButtonLogic(){
@@ -208,7 +220,7 @@ public class MainActivity extends AppCompatActivity {
         String listCode = list.getUuid();
         String finalListInfo = listName+" -> "+listCode;
         pantryList.add(finalListInfo);
-        listOperations();
+        pantryListSettings();
         Toast.makeText(MainActivity.this, "List added with success!", Toast.LENGTH_LONG).show();
     }
 
@@ -268,22 +280,137 @@ public class MainActivity extends AppCompatActivity {
     private void renderCreatePantryList(String token, String listName){
         String finalListInfo = listName+" -> "+token;
         pantryList.add(finalListInfo);
-        listOperations();
+        pantryListSettings();
         Toast.makeText(MainActivity.this, "List created with success!", Toast.LENGTH_LONG).show();
     }
 
-    private void fillTextView(){
-        TextView textView = findViewById(R.id.textView);
-        String username = getIntent().getStringExtra("username");
-        String email = getIntent().getStringExtra("email");
-        String info = "Welcome, ";
-        if(username.length()!=0){
-            info+=username;
-        }else{
-            info+="anonymous person";
-        }
-        textView.setText(info);
+
+    private void addShoppingListLogic(){
+        //add a shopping list through a list code
+        retrieveShoppingList();
+
+        //create custom shopping list
+        createShoppingList();
     }
 
+    private void retrieveShoppingList(){
+        Button getListButton = findViewById(R.id.getShoppingListButton);
+        getListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open get list dialog
+                handleGetShoppingListDialog();
+            }
+        });
+    }
+
+    private void handleGetShoppingListDialog(){
+        View view = getLayoutInflater().inflate(R.layout.get_pantry_list,null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view).show();
+        handleGetShoppingListLogic(view);
+    }
+
+    private void handleGetShoppingListLogic(View view){
+        Button getPantryListButton = view.findViewById(R.id.addPantryListButton);
+        getPantryListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText pantryListId = view.findViewById(R.id.getPantryListId);
+                String listId = pantryListId.getText().toString();
+                getShoppingListFromServer(listId);
+            }
+        });
+    }
+
+    private void getShoppingListFromServer(String listId){
+        String url = "/list/"+listId;
+        Call<ListServerData> call = retrofitInterface.getList(url);
+        call.enqueue(new Callback<ListServerData>() {
+            @Override
+            public void onResponse(Call<ListServerData> call, Response<ListServerData> response) {
+                if(response.code()==200){
+                    //list retrieved by the server
+                    ListServerData list = response.body();
+                    //render list in front-end
+                    renderGetShoppingList(list);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ListServerData> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "SERVER ERROR! Please try again later.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void renderGetShoppingList(ListServerData list){
+        String listName = list.getListName();
+        String listCode = list.getUuid();
+        String finalListInfo = listName+" -> "+listCode;
+        shoppingList.add(finalListInfo);
+        shoppingListSettings();
+        Toast.makeText(MainActivity.this, "List added with success!", Toast.LENGTH_LONG).show();
+    }
+
+
+    private void createShoppingList(){
+        Button createPantryListButton = findViewById(R.id.createShoppingListButton);
+        createPantryListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleCreateShoppingListDialog();
+            }
+        });
+    }
+
+    private void handleCreateShoppingListDialog(){
+        View view = getLayoutInflater().inflate(R.layout.create_pantry_list,null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view).show();
+        handleCreateShoppingListLogic(view);
+    }
+
+    private void handleCreateShoppingListLogic(View view){
+        Button getPantryListButton = view.findViewById(R.id.finalCreatePantryListButton);
+        getPantryListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText pantryListId = view.findViewById(R.id.newPantryListName);
+                String listName = pantryListId.getText().toString();
+                createShoppingListInServer(listName);
+            }
+        });
+    }
+
+    private void createShoppingListInServer(String listName){
+        HashMap<String,String> map = new HashMap<>();
+        map.put("name",listName);
+        Call<ServerListToken> call = retrofitInterface.executeListCreation(map);
+        call.enqueue(new Callback<ServerListToken>() {
+            @Override
+            public void onResponse(Call<ServerListToken> call, Response<ServerListToken> response) {
+                if(response.code()==200){
+                    //list retrieved by the server
+                    ServerListToken token = response.body();
+                    String tokenContent = token.getListId();
+                    //render list in front-end
+                    renderCreateShoppingList(tokenContent,listName);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ServerListToken> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "SERVER ERROR! Please try again later.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void renderCreateShoppingList(String token, String listName){
+        String finalListInfo = listName+" -> "+token;
+        shoppingList.add(finalListInfo);
+        shoppingListSettings();
+        Toast.makeText(MainActivity.this, "List created with success!", Toast.LENGTH_LONG).show();
+    }
 
 }
